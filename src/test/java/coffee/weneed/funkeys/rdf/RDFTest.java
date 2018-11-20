baf85458531d5ade883282cdadebd14435ed4132
Daleth Darko <daleth@Dalethium.com>
2018-11-20 13:36:43 +0100

    Changes and cleanups

--- src/test/java/coffee/weneed/funkeys/rdf/RDFTest.java
+++ src/test/java/coffee/weneed/funkeys/rdf/RDFTest.java
@@ -9,7 +9,9 @@ import coffee.weneed.utils.LogicUtil;
 public class RDFTest {
 
 	public static void main(String[] args) throws MalformedURLException, UnsupportedEncodingException {
-		System.out.println(RDFUtil.decode(new String(LogicUtil.downloadUrl(new File("G:\\UBFunkeys\\U.B. Funkeys 5.0 Full by Daleth\\U.B. Funkeys\\RadicaGame\\data\\system\\config.rdf").toURI().toURL()), "ISO-8859-1").toCharArray()));
-		
+		System.out.println(RDFUtil.decode(new String(LogicUtil.downloadUrl(
+				new File("G:\\UBFunkeys\\U.B. Funkeys 5.0 Full by Daleth\\U.B. Funkeys\\RadicaGame\\data\\system\\config.rdf").toURI().toURL()),
+				"ISO-8859-1")));
+
 	}
 }
